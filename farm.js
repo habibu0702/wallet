55485045a591be2e142f302967520da83eb74bef
TSB <habibusulaiman287@gmail.com>
2024-11-29 14:06:58 +0100

    Add files via upload

---
+++ farm.js
@@ -0,0 +1,5 @@
+
+let balance
+document.getElementById('clickme').addEventListener ('click', function() {
+    alert('you have');
+});
---
+++ wallet.js
@@ -0,0 +1,25 @@
+
+
+document.getElementById('holder').addEventListener ('click', function() {
+    alert('coming soon');
+});
+
+document.getElementById('receive').addEventListener ('click', function() {
+    alert('coming soon');
+});
+
+document.getElementById('send').addEventListener ('click', function() {
+    alert('coming soon');
+});
+
+document.getElementById('swap').addEventListener ('click', function() {
+    alert('coming soon');
+});
+
+document.getElementById('farm').addEventListener ('click', function() {
+    window.location.href = 'farm.html';
+});
+
+document.getElementById('add').addEventListener ('click', function() {
+    window.location.href = 'add.html';
+});
